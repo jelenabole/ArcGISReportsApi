840b254a703c3ed88179ba8ae98d54c45659dcf6
Bole <jelenabole@gmail.com>
2018-10-20 00:33:41 +0200

    new object

---
+++ ArcGisExportApi/Models/UrlForm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArcGisExportApi.Models
+{
+    public class UrlForm
+    {
+        public string Server { get; set; }
+        public string RestUrl { get; set; }
+    }
+}
