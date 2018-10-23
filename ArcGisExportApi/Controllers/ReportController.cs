29b9814821ada80b8af7945788e78a6777566619
Bole <jelenabole@gmail.com>
2018-10-23 21:39:18 +0200

    init report service

---
+++ ArcGisExportApi/Controllers/ReportController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using ArcGisExportApi.Models;
+using ArcGisExportApi.TestUtils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArcGisExportApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        // POST [Fromform]
+        [HttpPost]
+        public ActionResult<IEnumerable<string>> Post([FromBody] DataRequest request)
+        {
+
+            // do something
+
+            return new string[] { "value", "value" };
+        }
+    }
+}
---
+++ ArcGisExportApi/Models/DataRequest.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace ArcGisExportApi.Models
+{
+    // [DataContract]
+    public sealed class DataRequest
+    {
+        // [DataMember]
+        public List<SpatialCondition> SpatialConditionList;
+        // [DataMember]
+        public List<UrbanisticPlansResults> UrbanisticPlansResults;
+
+        public DataRequest()
+        {
+            SpatialConditionList = new List<SpatialCondition>();
+            UrbanisticPlansResults = new List<UrbanisticPlansResults>();
+        }
+    }
+
+    // for particles:
+    public class SpatialCondition
+    {
+        public string Source;
+        public string Type;
+        public string Description;
+        public Geometry Geometry;
+    }
+
+    public class Geometry
+    {
+        public List<List<List<double>>> Rings { get; set; }
+    }
+
+    // for map export:
+    public class UrbanisticPlansResults
+    {
+        public int Id;
+        public string Status;
+        public string Type;
+        public string GisCode;
+        public string Name;
+
+        // urls:
+        public string Sn;
+        public string PolygonRestURL;
+        public string RasterRestURL;
+        public string LegenRestURL;
+        public string ComponentRestURL;
+
+        public List<PlanMap> PlanMaps;
+
+        public class PlanMap
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public int MapScale { get; set; }
+            public int? OriginalScale { get; set; }
+        }
+    }
+}
